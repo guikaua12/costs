a839f6037b8142a41ff9e80dfc8b6b61d1ed3c9a
Approximations <datkidbosst@gmail.com>
2023-03-26 14:21:36 -0300

    Adicionado função de excluir projeto ao clicar em `Excluir`

--- frontend/src/components/ProjectCard/index.js
+++ frontend/src/components/ProjectCard/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './index.css';
 import ActionLink from '../ActionLink';
 import {FaPencilAlt, FaTrashAlt} from 'react-icons/fa';
 import ActionButton from '../ActionButton';
+import useAuth from '../../auth/useAuth';
 
-function ProjectCard({id, name, budget, type}) {
+function ProjectCard({id, name, budget, type, handleDelete}) {
     return (
         <div className='project-card'>
             <h1 className='card-title'>{name}</h1>
@@ -12,7 +13,7 @@ function ProjectCard({id, name, budget, type}) {
             <span className='type'>{type}</span>
             <div className='action-wrapper'>
                 <ActionLink icon={<FaPencilAlt></FaPencilAlt>} to={`/project/${id}`}>Editar</ActionLink>
-                <ActionButton icon={<FaTrashAlt></FaTrashAlt>}>Excluir</ActionButton>
+                <ActionButton icon={<FaTrashAlt></FaTrashAlt>} onClick={() => handleDelete(id)}>Excluir</ActionButton>
             </div>
         </div>
     );
--- frontend/src/pages/Projetos/index.js
+++ frontend/src/pages/Projetos/index.js
@@ -9,7 +9,7 @@ import Message from '../../components/Message';
 function Projetos() {
     const auth = useAuth();
     const location = useLocation();
-    const message = location.state && location.state.message;
+    const [message, setMessage] = useState(location.state ? location.state.message : undefined);
 
     const [projects, setProjects] = useState([]);
 
@@ -29,14 +29,39 @@ function Projetos() {
 
     }, []);
 
+    function deleteProject(id) {
+        const headers = {
+            authorization: `Bearer ${auth.getToken()}`
+        };
+
+        return fetch(`/projects/${id}`, {
+            method: 'DELETE',
+            headers
+        });
+    }
+
+    function handleDelete(id) {
+        deleteProject(id)
+            .then(response => response.json())
+            .then(data => {
+                if(data.erro) {
+                    console.log(data.msg);
+                    return;
+                }
+                setMessage(message => { return {type: 'success', msg: 'Projeto removido com sucesso.'}});
+                setProjects(projects => projects.filter(pr => pr._id !== id));
+            })
+            .catch(err => console.log(err));
+    }
+
     return (
         <Page title='Projetos' containerProps={{className: 'align-center justify-center block'}} currentPage='projetos'>
             {
-                message && <Message type={message.type} msg={message.msg}></Message>
+                message && <Message type='success' msg={message.msg}></Message>
             }
             <div className='projects-container'>
                 {
-                    projects.map(project => <ProjectCard key={project._id} id={project._id} name={project.name} budget={project.budget} type={project.category.name}></ProjectCard>)
+                    projects.map(project => <ProjectCard key={project._id} id={project._id} name={project.name} budget={project.budget} type={project.category.name} handleDelete={handleDelete}></ProjectCard>)
                 }
                 {/*<ProjectCard id='adaw' name='wawada' budget={100} type={1}></ProjectCard>*/}
 
