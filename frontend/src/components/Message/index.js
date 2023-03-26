98b51c5fef1c6bc929c486118158644fd6a17008
Approximations <datkidbosst@gmail.com>
2023-03-25 22:10:19 -0300

    Adicionado delay pra mensagem sumir

--- frontend/src/components/Message/index.js
+++ frontend/src/components/Message/index.js
@@ -1,11 +1,17 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './index.css';
 
-function Message({type, msg}) {
+function Message({type, msg, delay = 4000}) {
     const [message, setMessage] = useState({
         type,
         msg
     });
+
+    useEffect(() => {
+        setTimeout(() => {
+            setMessage(undefined);
+        }, delay);
+    }, []);
 
     return (
         message && <div className={`alert ${message.type}`}>
